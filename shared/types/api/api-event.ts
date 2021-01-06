155da8dc062a338d8875cf912e10424b656d52f3
Rotem Meiri <rotemx@gmail.com>
2021-01-06 14:08:07 +0200

    added update post feature + server init

--- shared/types/api/api-event.ts
+++ shared/types/api/api-event.ts
@@ -1,1 +1,1 @@
-export type APIEvent = 'getPosts' | 'createPost' | 'deletePost' | 'updatePost' | 'getPost';
+export type APIEvent = 'getPosts' | 'createPost' | 'deletePost' | 'updatePost' | 'getPost' | 'postsUpdate';
